cb0695f7e6b74fb34d634a5de4b8f04f79722c86
Kirill Karbutov <93286121+Karbutov00@users.noreply.github.com>
2022-04-20 21:39:01 -0400

    Create CoinItem.jsx

---
+++ src/components/CoinItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Coins.css";
+export default function CoinItem({ coin }) {
+    return (
+        <div className="coin-row">
+            <p>{coin.market_cap_rank}</p>
+            <div className="img-symbol">
+                <img src={coin.image} alt="" />
+                <p>{coin.symbol.toUpperCase()}</p>
+            </div>
+            <p>${coin.current_price.toLocaleString()}</p>
+            {coin.price_change_percentage_24h > 0 ? (
+                <p style={{ color: "#85bb65" }}>
+                    +{coin.price_change_percentage_24h}%
+                </p>
+            ) : (
+                <p style={{ color: "#F54040" }}>
+                    {coin.price_change_percentage_24h}%
+                </p>
+            )}
+
+            <p className="hide-mobile">{coin.total_volume.toLocaleString()}</p>
+            <p className="hide-mobile">${coin.market_cap.toLocaleString()}</p>
+        </div>
+    );
+}
